7a7bab9c3941cfdd7e72390d82bcdd7ab1ce69c0
aspin <tsunami70875@gmail.com>
2014-10-03 00:48:23 -0500

    project created

---
+++ remote-paper-prototyper-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remote-paper-prototyper-ios
+//
+//  Created by Kevin Chen on 10/3/14.
+//  Copyright (c) 2014 aspin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
