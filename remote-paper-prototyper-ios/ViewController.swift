0cb9c22febee572768f9b38beba4ad98cdb7edb0
aspin <tsunami70875@gmail.com>
2014-10-12 18:17:57 -0500

    added video player and touch recognition

--- remote-paper-prototyper-ios/ViewController.swift
+++ remote-paper-prototyper-ios/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import MediaPlayer
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var videoController : MPMoviePlayerController!
+    var tapGestureRecognizer : UITapGestureRecognizer!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.videoController = MPMoviePlayerController()
+        self.tapGestureRecognizer = UITapGestureRecognizer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        let filePath = NSBundle.mainBundle().pathForResource("demoVideo", ofType: "mp4")
+        let fileURL = NSURL.fileURLWithPath(filePath!)
+        
+        self.videoController.contentURL = fileURL
+        self.videoController.view.frame = CGRectMake(0, 0, 320,460)
+        self.videoController.controlStyle = MPMovieControlStyle.None
+        self.view.addSubview(self.videoController.view)
+        self.videoController.play()
 
-
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        var touch = event.allTouches()?.anyObject()
+        var touchPoint = touch?.locationInView(self.view)
+        println("x: \(touchPoint?.x)")
+        println("y: \(touchPoint?.y)")
+    }
 }
+
 
